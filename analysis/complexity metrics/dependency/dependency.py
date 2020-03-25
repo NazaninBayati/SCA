ea29a4b45e33e11a5c4605c99b86d940cd1859d6
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2020-03-25 14:30:12 +0430

    dependency

--- analysis/complexity metrics/dependency/dependency.py
+++ analysis/complexity metrics/dependency/dependency.py
@@ -44,17 +44,25 @@ dependency_dictionary.extend(local_variables)
 dependency_dictionary.extend(global_variables)
 dependency_dictionary.extend(local_function)
 
-
+call_by =[]
 #df = pandas.DataFrame(data=data)
 s1 = pd.Series(function_name, name='Function Name')
 s2 = pd.Series(function_type, name='Function Type')
 s3=pd.Series(local_variables, name ='local_variables')
 s4=pd.Series(global_variables, name ='Global_variables')
 s5=pd.Series(local_function, name ='local_Function')
+s6=pd.Series(call_by, name='Called By')
 #pd.set_option('display.max_colwidth', -1)
-pd.options.display.max_rows = 999
-pd.options.display.max_columns = 999
-df = pd.concat([s1,s2,s3,s4,s5], axis=1)
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
+
+pd.set_option('expand_frame_repr', True)
+df = pd.concat([s1,s2,s3,s4,s5,s6], axis=1)
 print(df)
 with open(' File Contents Report.txt', 'w') as filehandle:
     filehandle.write('%s\n' % df)
+
+def crossref(root_file, func):
+    {
+        print(i)
+    }
