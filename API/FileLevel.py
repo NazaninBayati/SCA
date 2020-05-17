9f9ed4f319d385ec848584bc1f7d5bc815888af4
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2020-05-17 16:18:14 +0430

    update on file level

--- API/FileLevel.py
+++ API/FileLevel.py
@@ -3,14 +3,41 @@ import sys
 
 
 final_set=[]
+report=[]
+list_db = []
+checklist = []
+db_func=[]
+def containedfunctions(db_file):
+    func_name = []
+    for func in db.ents("function"):
+
+        func_name.append(func.longname())
+        func_root_file = func_name[0].split(":")
+        if func_root_file[0] == db_file:
+            db_func.append(func)
+        print(func_name)
+    return db_func
+
+
+def containedclasses():
+    cls_name = []
+    for cls in db.ents("class,Class,classes"):
+
+        cls_name.append(cls.longname())
+        print(cls_name)
+    return cls_name
+
+
 def fileList(db):
     for file in db.ents("File"):
         file_name = []
+        file_qname = []
         # If file is from the Ada Standard library, skip to next
         if file.library() != "Standard":
             file_name.append(file.name())
 
             db_analyze_pfix = file_name[0].split('.')
+            cont_func = containedfunctions(db_analyze_pfix[0])
             if len(db_analyze_pfix)>1 and( db_analyze_pfix[1] == 'cpp' or db_analyze_pfix[1] == 'h' or db_analyze_pfix[1] == 'C' or \
                     db_analyze_pfix[1] == 'hpp' or db_analyze_pfix[1] == 'hxx' or db_analyze_pfix[1] == 'cxx' or \
                     db_analyze_pfix[1] == 'H' or db_analyze_pfix[1] == 'inl' or db_analyze_pfix[1] == 'cc' or \
@@ -77,50 +104,84 @@ def fileList(db):
                     db_analyze_language = 'Php'
             file_lang=[]
             file_lang.append(db_analyze_language)
+            file_qname= file.longname()
+            file_name_Qualified=[]
+            file_name_Qualified = file_qname.split('/')
+            last=[]
 
-        final_set.append(file_name)
-        final_set.append(file_lang)
-        #metric=''
-        ##metric = (projectMetrics(file_name))
-        #final_set.append(metric)
+            last = file_name_Qualified[file_name_Qualified.__len__()-1].split('.')
+            location=[]
+
+            location = ".".join(file_name_Qualified[0:file_name_Qualified.__len__() - 1])
+            qlast=[]
+            qlast.append(location)
+            qlast.append(last[0])
+            file_name_Qualified='.'.join(qlast[0:qlast.__len__()])
+            file_metric = []
+            metrics = file.metric(file.metrics())
+
+            for k, v in sorted(metrics.items()):
+
+                file_metric.append(v)
+            #print(file.ents("Class"))
+
+
+        final_set.append(file_name[0])
+        final_set.append(file_lang[0])
+        final_set.append(file_name_Qualified)
+        final_set.append(location)
+        final_set.append(file_metric)
+        final_set.append(cont_func)
+
+
+
+
+
+        report.append(final_set)
+
 
 file=[]
-def projectMetrics(db):
-    i = 0
-    for i in range( len(db)):
-
-        metrics = db[i].metric(db[i].metrics())
-        for k, v in sorted(metrics.items()):
-           print(k, "=", v)
-           file.append(db[i])
-           file.append(k)
-           file.append(v)
-           #file.append("\n")
-           final_set.append(file)
-           file=[]
-    print("For metric details see: http://www.scitools.com/documents/metrics.php")
-    #return  file
+final=[]
 
 
-file_metric=[]
+class_db=[]
+function_db=[]
 metr=[]
 if __name__ == '__main__':
     # Open Database
     args = sys.argv
-    db = understand.open("/home/nazanin/ceph.udb")
+    db = understand.open("/home/nazanin/New.udb")
+
+    #function_db=containedfunctions()
+    class_db = containedclasses()
 
     fileList(db)
-    metr = projectMetrics(db.ents("File"))
+    fil_list=[]
+    file_list= db.ents('File')
 
-print(metr)
+
+
+#print(metr)
 File_header=[]
-File_header=['Name','ProgrammingLanguage','qualifiedName','location', 'Lines','CommentLines','BlankLines','PreprocessorLines','CodeLines','InactiveLines','ExecutableCodeLines','DeclarativeCodeLines', 'ExecutionStatements',  'DeclarationStatements',  'RatioComment/Code', 'Units', 'ontainedGlobalVariables', 'containedClasses','containedFunctions','usesSourceFiles','usedbySourceFiles']
-final_set.insert(0,File_header)
-i = 0
-with open('File Level Report.txt', 'w') as filehandle:
+File_header=['Name','ProgrammingLanguage','qualifiedName','location', 'Lines','CommentLines','BlankLines','PreprocessorLines','CodeLines','InactiveLines','ExecutableCodeLines','DeclarativeCodeLines', 'ExecutionStatements',  'DeclarationStatements',  'RatioComment/Code', 'Units', 'containedClasses','containedFunctions','usesSourceFiles','usedbySourceFiles']
+#report.insert(0,File_header)
 
-    while i < final_set.__len__():
-       for listitem in final_set[i]:
+i=0
+with open('FileLevel Report.txt', 'w') as classhandle:
+
+    #while i < final_set.__len__():
+       for listitem in report[i]:
+            classhandle.write('%s;' % listitem)
+            i= i +1
+            if i %5 == 0:            classhandle.write('\n')
+       #i = i+1
+
+i=0
+with open('File Level classes Report.txt', 'w') as filehandle:
+
+    while i < report.__len__():
+       for listitem in class_db:
             filehandle.write('%s;' % listitem)
+            #if i %4 == 0: filehandle.write('\n')
        filehandle.write('\n')
        i = i+1
