d963475a2f3934a36ae33c508106c95dc262d802
NaziB <54472723+NazaninBayati@users.noreply.github.com>
2020-07-12 14:36:07 +0430

    update

--- pydrill/arm.py
+++ pydrill/arm.py
@@ -3,15 +3,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 
-store_data = pd.read_csv("/home/nazanin/modified_DB_Ceph.csv", header=None, dtype="string")
-num_records = len(store_data)
-print(num_records)
-records = []
-for i in range(0, num_records):
-    records.append([str(store_data.values[i, j])for j in range(0, 20)])
+temp = []
 
-association_rules = apriori(records, min_support=0.005, min_confidence=0.20)
+store_data = pd.read_csv("/home/nazanin/modified_DB_Ceph_summary.csv", header=None, dtype="string")
+dataset = store_data.values.tolist()
+
+#transactions = [    ['beer', 'nuts'],    ['beer', 'cheese'],    ['beer', 'cheese','coca']]
+#results = list(apriori(transactions))
+
+
+#data cleaning
+for m in range(len(dataset)):
+    temp = dataset[m]
+    for n in range(len(dataset[m])):
+        if str(dataset[m][n] ) == '<NA>':
+            counter = n
+            dataset[m] = temp[0:n]
+            break
+print(dataset)
+
+association_rules = apriori(dataset, min_support=0.005, min_confidence=0.20)
 association_rules = list(association_rules)
 print(len(association_rules))
 print(association_rules)
-
